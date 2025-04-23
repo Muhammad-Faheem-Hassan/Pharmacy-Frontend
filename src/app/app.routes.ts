394630a3651beb515e22c6f9f1f4aab5cff0d2e3
Faheem Hassan <faheemhassan542@gmail.com>
2025-04-23 04:53:19 -0700

    Purchase form fixed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,18 +9,25 @@ import { PurchaseListComponent } from './pages/purchases/purchases-list/purchase
 import { SaleListComponent } from './pages/sales/sales-list/sales-list.component';
 import { SaleFormComponent } from './pages/sales/sales-form/sales-form.component';
 import { SalesDetailsComponent } from './pages/sales/sales-details/sales-details.component';
+import { PurchasesDetailsComponent } from './pages/purchases/purchases-details/purchases-details.component';
 
 export const routes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'dashboard', component: DashboardComponent },
+  
   { path: 'medicines', component: MedicinesListComponent },
   { path: 'medicines/form', component: MedicinesComponent },
+
   { path: 'sale', component: SaleListComponent },
   { path: 'sale/form', component: SaleFormComponent },
   { path: 'sale/form/:mode', component: SaleFormComponent },
   { path: 'sale/details/:id', component: SalesDetailsComponent },
+  
   { path: 'purchase', component: PurchaseListComponent },
   { path: 'purchase/form', component: PurchaseFormComponent },
+  { path: 'purchase/form/:mode', component: PurchaseFormComponent },
+  { path: 'purchase/details/:id', component: PurchasesDetailsComponent },
+
   { path: 'supplier', component: SupplierListComponent },
   { path: 'supplier/form', component: SupplierFormComponent },
 ];
--- src/app/pages/purchases/purchases-list/purchases-list.component.ts
+++ src/app/pages/purchases/purchases-list/purchases-list.component.ts
@@ -2,18 +2,32 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { PurchaseService } from '../../../core/services/puchase.service';
+import { PaginationComponent } from '../../../shared/pagination/pagination.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-purchase-list',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    PaginationComponent,
+    RouterLink],
   templateUrl: './purchases-list.component.html',
 })
 export class PurchaseListComponent implements OnInit {
   purchases: any[] = [];
   isLoading = true;
 
-  constructor(private purchaseService: PurchaseService) {}
+  totalRecord = 0;
+  currentPage = 1;
+  pageSize = 10;
+
+  startDate: string = '';
+  endDate: string = '';
+  type: string = '';
+
+  constructor(private purchaseService: PurchaseService) { }
 
   ngOnInit(): void {
     this.fetchPurchases();
@@ -21,11 +35,38 @@ export class PurchaseListComponent implements OnInit {
 
   async fetchPurchases(): Promise<void> {
     try {
-      this.purchases = await this.purchaseService.fetch();
+      const query: any = {
+        l: this.pageSize,
+        o: (this.currentPage - 1) * this.pageSize,
+      };
+      if (this.startDate) {
+        query.startDate = this.startDate;
+      }
+
+      if (this.endDate) {
+        query.endDate = this.endDate;
+      }
+
+      if (this.type) {
+        query.type = this.type;
+      }
+      const res = await this.purchaseService.fetch(query);
+      this.purchases = res.data;
+      this.totalRecord = res.total;
     } catch (error) {
       console.error('Error fetching purchases:', error);
     } finally {
       this.isLoading = false;
     }
+  }
+
+  onPageChange(page: number) {
+    this.currentPage = page;
+    this.fetchPurchases();
+  }
+
+  onFilterChange() {
+    this.currentPage = 1;
+    this.fetchPurchases();
   }
 }
